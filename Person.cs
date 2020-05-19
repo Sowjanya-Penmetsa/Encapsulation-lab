4dde037d483fa2c02475e12efda779fd9c1c7a2a
unknown <penmetsa.sowjanya30@gmail.com>
2020-05-19 15:37:21 +0200

    first commit

---
+++ Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation_lab
+{
+    class Person
+    {
+        
+        public Person(string firstname, string lastname, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+        }
+
+        private string firstname;
+        private string lastname;
+        private int age;
+
+        public string Firstname
+        {
+            get
+            {
+                return this.firstname;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return this.lastname;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstname}  {this.lastname} is {this.age} years old";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Encapsulation_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = 5;
+
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+
+            {
+
+                var cmdArgs = Console.ReadLine().Split();
+
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+
+                persons.Add(person);
+
+            }
+            Console.ReadLine();
+
+            persons.OrderBy(p => p.Firstname)
+
+            .ThenBy(p => p.Age)
+
+            .ToList()
+
+            .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
